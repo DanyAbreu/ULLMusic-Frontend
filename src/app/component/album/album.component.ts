23839eec8fe25612100e50d0098588cb9ed87733
DanyAbreu <daniel_95_a@hotmail.es>
2024-06-15 18:52:55 +0100

    Footer fix and tracks list

--- src/app/component/album/album.component.ts
+++ src/app/component/album/album.component.ts
@@ -12,25 +12,29 @@ import { Router } from '@angular/router';
 export class AlbumComponent {
 
   idAlb!: string;
+  album!: any;
+  currentPage = 1;
+  tracksPerPage = 10;
+
   constructor(private router: Router, private route: ActivatedRoute, private DataService: DataService){}
-  album!: {
-    idAlb: string;
-    artists: [{
-    idArt: string;
-    nameArt: string;
-    }];
-    nameAlb: string;
-    imageAlbUrl: string;
-    genresAlb: string;
-    popularityAlb: number;
-    releaseDate: Date;
-    tracks: [{
-      idTrack: string;
-      nameTrack: string;
-      previewUrl: string;
-      duration: Number;
-    }];
-  };
+
+  get paginatedTracks() {
+    const startIndex = (this.currentPage - 1) * this.tracksPerPage;
+    const endIndex = startIndex + this.tracksPerPage;
+    return this.album.tracks.slice(startIndex, endIndex);
+  }
+
+  get totalPages() {
+    return Math.ceil(this.album.tracks.length / this.tracksPerPage);
+  }
+
+  get pages() {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+  changePage(page: number) {
+    this.currentPage = page;
+  }
 
   navArtist (idArt:string){
     this.router.navigate(['/artist/',idArt]);
@@ -45,6 +49,8 @@ export class AlbumComponent {
         this.DataService.getAlbum(this.idAlb).subscribe(
           (data) => {
             this.album = data;
+            console.log(this.album.artists);
+            console.log(this.album.tracks);
           }
         );
       }
--- src/app/component/artist/artist.component.ts
+++ src/app/component/artist/artist.component.ts
@@ -11,8 +11,28 @@ import { DataService } from 'src/app/servicies/back/data.service';
 export class ArtistComponent {
   idArt!: string;
   artist!: any;
+  currentPage = 1;
+  tracksPerPage = 10;
 
   constructor(private route: ActivatedRoute, private router: Router, private DataService: DataService){}
+
+  get paginatedTracks() {
+    const startIndex = (this.currentPage - 1) * this.tracksPerPage;
+    const endIndex = startIndex + this.tracksPerPage;
+    return this.artist.tracks.slice(startIndex, endIndex);
+  }
+
+  get totalPages() {
+    return Math.ceil(this.artist.tracks.length / this.tracksPerPage);
+  }
+
+  get pages() {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+  changePage(page: number) {
+    this.currentPage = page;
+  }
 
   // Metodo para navegar al componente Album
   navAlbum (idAlb:string){
@@ -26,11 +46,9 @@ export class ArtistComponent {
         this.DataService.getArtist(this.idArt).subscribe(
           (data) => {
             this.artist = data;
-
             const contentDiv = document.getElementById('content');
-            console.log(contentDiv);
             if (contentDiv) {
-              contentDiv.innerHTML = this.artist.content;
+              contentDiv.innerHTML = this.artist.summary;
             }
           }
         )
